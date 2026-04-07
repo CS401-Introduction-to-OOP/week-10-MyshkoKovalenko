6c2106b841a9897baa0cb56a8bb5eaaf73891fa0
Mykhailo Kovalenko <mykhaylo2007@gmail.com>
2026-04-07 18:27:46 +0300

    Added Character and Event classes

---
+++ Character.cs
@@ -0,0 +1,30 @@
+class Character
+{
+    public string Name { get; }
+
+    public Role Role { get; }
+
+    public int Level { get; }
+
+    public int HP { get; }
+
+    public int Gold { get; }
+
+    public bool IsAlive { get => HP > 0; }
+
+    Character(string name, Role role)
+    {
+        Name = name;
+        Role = role;
+        Level = 1;
+        HP = 100;
+        Gold = 0;
+    } 
+}
+
+enum Role
+{
+    Warrior,
+    Archer,
+    Wizard
+}
---
+++ Event.cs
@@ -0,0 +1,28 @@
+class Event
+{
+    static int nextTurn = 1;
+
+    public int TurnNumber { get; }
+
+    public string Description { get; }
+
+    public EventType Type { get; }
+
+    public int Value { get; }
+
+    Event(string description, EventType type, int value)
+    {
+        TurnNumber = nextTurn++;
+        Description = description;
+        Type = type;
+        Value = value;
+    }
+}
+
+enum EventType
+{
+    Attack,
+    Heal,
+    LevelUp,
+    Loot
+}
